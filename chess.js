a7ea2fa02438f95fcf1a597818d099d70e679c0b
Worst-in-Me <ivan_kostin@lenta.ru>
2023-05-23 22:48:07 +0300

    wrote function createElem, createChessBoard

--- chess.js
+++ chess.js
@@ -1,5 +1,16 @@
 const wrapper = document.querySelector('.wrapper');
 
+const letters = {
+    1: 'a',
+    2: 'b',
+    3: 'c',
+    4: 'd',
+    5: 'e',
+    6: 'f',
+    7: 'g',
+    8: 'h',
+};
+
 const createElem = (className, tagName, content) => {
     const elem = document.createElement(tagName);
     elem.classList.add(className);
@@ -12,16 +23,23 @@ const createChessBoard = (elem) => {
     const drawBoard = () => {
         const chessBoard = createElem('chessBoard', 'table');
 
+        let count = 8;
         for (let i = 0; i < 8; i++) {
             if (i % 2 == 0) {
                 const tr = createElem('tableRow', 'tr');
 
-                for (let i = 0; i < 8; i++) {
-                    if (i % 2 !== 0) {
+                for (let j = 0; j < 8; j++) {
+                    if (j % 2 !== 0) {
                         const td = createElem('blackCell', 'td');
                         tr.appendChild(td);
+
+                        if (j == 7) {
+                            td.textContent = count;
+                            count--;
+                        }
                     } else {
                         const td = createElem('cell', 'td');
+
                         tr.appendChild(td);
                     }
                 }
@@ -30,13 +48,22 @@ const createChessBoard = (elem) => {
             } else {
                 const tr = createElem('tableRow', 'tr');
 
-                for (let i = 0; i < 8; i++) {
-                    if (i % 2 == 0) {
+                for (let j = 0; j < 8; j++) {
+                    if (j % 2 == 0) {
                         const td = createElem('blackCell', 'td');
                         tr.appendChild(td);
+
+                        if (i == 7) td.textContent = letters[j + 1];
                     } else {
                         const td = createElem('cell', 'td');
                         tr.appendChild(td);
+
+                        if (i == 7) td.textContent = letters[j + 1];
+
+                        if (j == 7) {
+                            td.textContent = count;
+                            count--;
+                        }
                     }
                 }
 
