71734d422c29f46fc939f52c3cfc98643e28e51d
Worst-in-Me <ivan_kostin@lenta.ru>
2023-05-23 23:40:37 +0300

    wrote function createElem, createChessBoard

--- chess.js
+++ chess.js
@@ -1,16 +1,5 @@
 const wrapper = document.querySelector('.wrapper');
 
-const letters = {
-    1: 'a',
-    2: 'b',
-    3: 'c',
-    4: 'd',
-    5: 'e',
-    6: 'f',
-    7: 'g',
-    8: 'h',
-};
-
 const createElem = (className, tagName, content) => {
     const elem = document.createElement(tagName);
     elem.classList.add(className);
@@ -20,10 +9,12 @@ const createElem = (className, tagName, content) => {
 };
 
 const createChessBoard = (elem) => {
+    const letters = createElem('lettersRow', 'div', 'A B C D E F G H');
+
     const drawBoard = () => {
         const chessBoard = createElem('chessBoard', 'table');
+        const numbers = createElem('numbersColumn', 'div', '8 7 6 5 4 3 2 1');
 
-        let count = 8;
         for (let i = 0; i < 8; i++) {
             if (i % 2 == 0) {
                 const tr = createElem('tableRow', 'tr');
@@ -32,11 +23,6 @@ const createChessBoard = (elem) => {
                     if (j % 2 !== 0) {
                         const td = createElem('blackCell', 'td');
                         tr.appendChild(td);
-
-                        if (j == 7) {
-                            td.textContent = count;
-                            count--;
-                        }
                     } else {
                         const td = createElem('cell', 'td');
 
@@ -52,18 +38,9 @@ const createChessBoard = (elem) => {
                     if (j % 2 == 0) {
                         const td = createElem('blackCell', 'td');
                         tr.appendChild(td);
-
-                        if (i == 7) td.textContent = letters[j + 1];
                     } else {
                         const td = createElem('cell', 'td');
                         tr.appendChild(td);
-
-                        if (i == 7) td.textContent = letters[j + 1];
-
-                        if (j == 7) {
-                            td.textContent = count;
-                            count--;
-                        }
                     }
                 }
 
@@ -71,11 +48,14 @@ const createChessBoard = (elem) => {
             }
         }
 
-        return chessBoard;
+        const container = createElem('container', 'div');
+        container.append(chessBoard, numbers);
+
+        return container;
     };
 
     const board = drawBoard();
-    elem.appendChild(board);
+    elem.append(board, letters);
 };
 
 createChessBoard(wrapper);
