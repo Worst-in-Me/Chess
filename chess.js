477a89386d6a4463373a276ba97139843a15e187
Worst-in-Me <ivan_kostin@lenta.ru>
2023-06-10 17:04:06 +0300

    figures possible moves done

--- chess.js
+++ chess.js
@@ -2,36 +2,42 @@ const wrapper = document.querySelector('.wrapper');
 
 const figures = {
     p: {
+        name: 'pawn',
         model: './images/figures/black_pawn.svg',
 
         check() {},
         move() {},
     },
     k: {
+        name: 'king',
         model: './images/figures/black_king.svg',
 
         check() {},
         move() {},
     },
     q: {
+        name: 'queen',
         model: './images/figures/black_queen.svg',
 
         check() {},
         move() {},
     },
     b: {
+        name: 'bishop',
         model: './images/figures/black_bishop.svg',
 
         check() {},
         move() {},
     },
     n: {
+        name: 'knight',
         model: './images/figures/black_unicorn.svg',
 
         check() {},
         move() {},
     },
     r: {
+        name: 'rook',
         model: './images/figures/black_rook.svg',
 
         check() {},
@@ -79,6 +85,8 @@ const initPos = {
 
 const posibleMoves = [];
 
+const allCell = [];
+
 let pickedFigure = '';
 
 const currPos = { ...initPos };
@@ -86,7 +94,7 @@ const currPos = { ...initPos };
 const clearPosibleMoves = () => {
     posibleMoves.forEach((item) => {
         const a = document.querySelector(`[cell=${item}]`);
-        console.log(a);
+        // console.log(a);
 
         if (a.classList.contains('targetCell')) a.classList.remove('targetCell');
     });
@@ -119,8 +127,254 @@ const pawnCheck = (currCell, figureData) => {
         const a = document.querySelector(`[cell=${item}]`);
         a.classList.toggle('targetCell');
     });
-    console.log(posibleMoves);
+    // console.log(posibleMoves);
 };
+
+const knightCheck = (currCell) => {
+    const cell = currCell.getAttribute('cell').split('');
+    const buf = [];
+
+    buf.push(String.fromCharCode(cell[0].charCodeAt(0) + 1) + (+cell[1] + 2));
+    buf.push(String.fromCharCode(cell[0].charCodeAt(0) + 1) + (+cell[1] - 2));
+    buf.push(String.fromCharCode(cell[0].charCodeAt(0) - 1) + (+cell[1] + 2));
+    buf.push(String.fromCharCode(cell[0].charCodeAt(0) - 1) + (+cell[1] - 2));
+    buf.push(String.fromCharCode(cell[0].charCodeAt(0) + 2) + (+cell[1] + 1));
+    buf.push(String.fromCharCode(cell[0].charCodeAt(0) + 2) + (+cell[1] - 1));
+    buf.push(String.fromCharCode(cell[0].charCodeAt(0) - 2) + (+cell[1] + 1));
+    buf.push(String.fromCharCode(cell[0].charCodeAt(0) - 2) + (+cell[1] - 1));
+
+    for (let i = 0; i < 8; i++) if (allCell.includes(buf[i]) && !currPos[buf[i]]) posibleMoves.push(buf[i]);
+
+    posibleMoves.forEach((item) => {
+        const a = document.querySelector(`[cell=${item}]`);
+
+        if (allCell.includes(item)) a.classList.toggle('targetCell');
+    });
+};
+
+const rookCheck = (currCell) => {
+    const buf = [];
+
+    let cell1 = currCell.getAttribute('cell');
+    while (allCell.includes(cell1)) {
+        cell1 = cell1.split('');
+        cell1[1] = +cell1[1] + 1;
+        cell1 = cell1.join('');
+
+        if (cell1 in currPos) break;
+
+        buf.push(cell1[0] + cell1[1]);
+    }
+
+    let cell2 = currCell.getAttribute('cell');
+    while (allCell.includes(cell2)) {
+        cell2 = cell2.split('');
+        cell2[1] = +cell2[1] - 1;
+        cell2 = cell2.join('');
+
+        if (cell2 in currPos) break;
+
+        buf.push(cell2[0] + cell2[1]);
+    }
+
+    let cell3 = currCell.getAttribute('cell');
+    while (allCell.includes(cell3)) {
+        cell3 = cell3.split('');
+        cell3[0] = String.fromCharCode(cell3[0].charCodeAt(0) + 1);
+        cell3 = cell3.join('');
+
+        if (cell3 in currPos) break;
+
+        buf.push(cell3[0] + cell3[1]);
+    }
+
+    let cell4 = currCell.getAttribute('cell');
+    while (allCell.includes(cell4)) {
+        cell4 = cell4.split('');
+        cell4[0] = String.fromCharCode(cell4[0].charCodeAt(0) - 1);
+        cell4 = cell4.join('');
+
+        if (cell4 in currPos) break;
+
+        buf.push(cell4[0] + cell4[1]);
+    }
+
+    for (let i = 0; i < buf.length; i++) {
+        if (allCell.includes(buf[i])) posibleMoves.push(buf[i]);
+    }
+
+    posibleMoves.forEach((item) => {
+        const a = document.querySelector(`[cell=${item}]`);
+
+        if (allCell.includes(item)) a.classList.toggle('targetCell');
+    });
+};
+
+const bishopCheck = (currCell) => {
+    const buf = [];
+
+    let cell1 = currCell.getAttribute('cell');
+    while (allCell.includes(cell1)) {
+        cell1 = cell1.split('');
+        cell1[1] = +cell1[1] + 1;
+        cell1[0] = String.fromCharCode(cell1[0].charCodeAt(0) + 1);
+        cell1 = cell1.join('');
+
+        if (cell1 in currPos) break;
+
+        buf.push(cell1[0] + cell1[1]);
+    }
+
+    let cell2 = currCell.getAttribute('cell');
+    while (allCell.includes(cell2)) {
+        cell2 = cell2.split('');
+        cell2[1] = +cell2[1] + 1;
+        cell2[0] = String.fromCharCode(cell2[0].charCodeAt(0) - 1);
+        cell2 = cell2.join('');
+
+        if (cell2 in currPos) break;
+
+        buf.push(cell2[0] + cell2[1]);
+    }
+
+    let cell3 = currCell.getAttribute('cell');
+    while (allCell.includes(cell3)) {
+        cell3 = cell3.split('');
+        cell3[0] = String.fromCharCode(cell3[0].charCodeAt(0) + 1);
+        cell3[1] = +cell3[1] - 1;
+        cell3 = cell3.join('');
+
+        if (cell3 in currPos) break;
+
+        buf.push(cell3[0] + cell3[1]);
+    }
+
+    let cell4 = currCell.getAttribute('cell');
+    while (allCell.includes(cell4)) {
+        cell4 = cell4.split('');
+        cell4[0] = String.fromCharCode(cell4[0].charCodeAt(0) - 1);
+        cell4[1] = +cell4[1] - 1;
+        cell4 = cell4.join('');
+
+        if (cell4 in currPos) break;
+
+        buf.push(cell4[0] + cell4[1]);
+    }
+
+    for (let i = 0; i < buf.length; i++) if (allCell.includes(buf[i])) posibleMoves.push(buf[i]);
+
+    posibleMoves.forEach((item) => {
+        const a = document.querySelector(`[cell=${item}]`);
+
+        if (allCell.includes(item)) a.classList.toggle('targetCell');
+    });
+};
+
+const queenCheck = (currCell) => {
+    const buf = [];
+
+    let cell1 = currCell.getAttribute('cell');
+    while (allCell.includes(cell1)) {
+        cell1 = cell1.split('');
+        cell1[1] = +cell1[1] + 1;
+        cell1 = cell1.join('');
+
+        if (cell1 in currPos) break;
+
+        buf.push(cell1[0] + cell1[1]);
+    }
+
+    let cell2 = currCell.getAttribute('cell');
+    while (allCell.includes(cell2)) {
+        cell2 = cell2.split('');
+        cell2[1] = +cell2[1] - 1;
+        cell2 = cell2.join('');
+
+        if (cell2 in currPos) break;
+
+        buf.push(cell2[0] + cell2[1]);
+    }
+
+    let cell3 = currCell.getAttribute('cell');
+    while (allCell.includes(cell3)) {
+        cell3 = cell3.split('');
+        cell3[0] = String.fromCharCode(cell3[0].charCodeAt(0) + 1);
+        cell3 = cell3.join('');
+
+        if (cell3 in currPos) break;
+
+        buf.push(cell3[0] + cell3[1]);
+    }
+
+    let cell4 = currCell.getAttribute('cell');
+    while (allCell.includes(cell4)) {
+        cell4 = cell4.split('');
+        cell4[0] = String.fromCharCode(cell4[0].charCodeAt(0) - 1);
+        cell4 = cell4.join('');
+
+        if (cell4 in currPos) break;
+
+        buf.push(cell4[0] + cell4[1]);
+    }
+
+    let cell5 = currCell.getAttribute('cell');
+    while (allCell.includes(cell5)) {
+        cell5 = cell5.split('');
+        cell5[1] = +cell5[1] + 1;
+        cell5[0] = String.fromCharCode(cell5[0].charCodeAt(0) + 1);
+        cell5 = cell5.join('');
+
+        if (cell5 in currPos) break;
+
+        buf.push(cell5[0] + cell5[1]);
+    }
+
+    let cell6 = currCell.getAttribute('cell');
+    while (allCell.includes(cell6)) {
+        cell6 = cell6.split('');
+        cell6[1] = +cell6[1] + 1;
+        cell6[0] = String.fromCharCode(cell6[0].charCodeAt(0) - 1);
+        cell6 = cell6.join('');
+
+        if (cell6 in currPos) break;
+
+        buf.push(cell6[0] + cell6[1]);
+    }
+
+    let cell7 = currCell.getAttribute('cell');
+    while (allCell.includes(cell7)) {
+        cell7 = cell7.split('');
+        cell7[0] = String.fromCharCode(cell7[0].charCodeAt(0) + 1);
+        cell7[1] = +cell7[1] - 1;
+        cell7 = cell7.join('');
+
+        if (cell7 in currPos) break;
+
+        buf.push(cell7[0] + cell7[1]);
+    }
+
+    let cell8 = currCell.getAttribute('cell');
+    while (allCell.includes(cell8)) {
+        cell8 = cell8.split('');
+        cell8[0] = String.fromCharCode(cell8[0].charCodeAt(0) - 1);
+        cell8[1] = +cell8[1] - 1;
+        cell8 = cell8.join('');
+
+        if (cell8 in currPos) break;
+
+        buf.push(cell8[0] + cell8[1]);
+    }
+
+    for (let i = 0; i < buf.length; i++) if (allCell.includes(buf[i])) posibleMoves.push(buf[i]);
+
+    posibleMoves.forEach((item) => {
+        const a = document.querySelector(`[cell=${item}]`);
+
+        if (allCell.includes(item)) a.classList.toggle('targetCell');
+    });
+};
+
+const kingCheck = (currCell) => {};
 
 const createElem = (className, tagName, content, attrs = {}) => {
     const elem = document.createElement(tagName);
@@ -158,11 +412,14 @@ const createChessBoard = (elem) => {
             for (let j = 0; j < 8; j++) {
                 const td = createElem(`cell ${i % 2 == j % 2 ? '' : 'blackCell'}`, 'div', '', {
                     cell: String.fromCharCode(65 + j) + i,
+                    piece: '',
                 });
                 const figure = initPos[String.fromCharCode(65 + j) + i];
+                allCell.push(String.fromCharCode(65 + j) + i);
 
                 if (figure) {
                     const img = new Image();
+                    td.setAttribute('piece', figures[figure[0][0]].name);
                     img.src = figures[figure[0][0]].model;
                     if (figure[0][2] === 'w') img.style = 'filter: invert(100%)';
                     td.appendChild(img);
@@ -192,7 +449,25 @@ const createChessBoard = (elem) => {
                     cell.classList.add('targetFigure');
                     pickedFigure = cell.getAttribute('cell');
 
-                    pawnCheck(cell, currPos[cell.getAttribute('cell')]);
+                    switch (cell.getAttribute('piece')) {
+                        case 'pawn':
+                            pawnCheck(cell, currPos[cell.getAttribute('cell')]);
+                            break;
+                        case 'knight':
+                            knightCheck(cell);
+                            break;
+                        case 'rook':
+                            rookCheck(cell);
+                            break;
+                        case 'bishop':
+                            bishopCheck(cell);
+                            break;
+                        case 'queen':
+                            queenCheck(cell);
+                            break;
+                    }
+                    // pawnCheck(cell, currPos[cell.getAttribute('cell')]);
+                    // knightCheck(cell);
                 }
             } else {
                 // if (posibleMoves.includes(cell.getAttribute('cell'))) {}
